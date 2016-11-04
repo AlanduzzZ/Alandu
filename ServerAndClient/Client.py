9a0596812c493cbbd3cd3a6026ac6c92960081a2
imalandu <dovequn@foxmail.com>
2016-11-04 20:59:17 +0800

    添加异常判断

--- ServerAndClient/Client.py
+++ ServerAndClient/Client.py
@@ -7,18 +7,29 @@ host = '127.0.0.1'
 port = 20001
 addr = (host, port)
 Client = socket(AF_INET, SOCK_STREAM)
-Client.connect(addr)
+try:
+    Client.connect(addr)
+except ConnectionRefusedError:
+    print('ConnectionRefusedError: Server(%s)端口(%s)未打开或连接被拒绝' % (host, port))
+except ConnectionResetError:
+    print('ConnectionResetError: Server(%s)强制关闭连接' % host)
+
 while True:
     data = input('Please input the data :')
     if not data or data==r'exit' or data==r'EXIT':
         break
     senddata = '%s\n' % data
-    print(r'Your senddata is %s' % senddata)
-    Client.sendall(senddata.encode())
-    respdata = Client.recv(1024)
+    print(r'Your senddata is %s' % senddata.strip())
+    try:
+        Client.sendall(senddata.encode())
+        respdata = Client.recv(1024)
+    except OSError:
+        print('OSError: Client与Server(%s)端口(%s)的连接未建立' % (host, port))
+        respdata = b''
+        pass
     if not respdata:
         print(r'Server response is Empty !')
-        break
+        pass
     else:
-        print(r'Your respdata is %s' % respdata.decode())
+        print(r'Your respdata is %s' % respdata.decode().strip())
 Client.close()
